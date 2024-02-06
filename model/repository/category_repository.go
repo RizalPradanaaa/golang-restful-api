5b2cb23a3a68c3dfdf9a2b3059d22a22e3d4dbff
RizalPradanaaa <rizalpradanaaaa@gmail.com>
2024-02-06 09:02:14 +0700

    Category Repository

---
+++ model/repository/category_repository.go
@@ -0,0 +1,17 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"golang-restful-api/model/domain"
+)
+
+// Interface / Kontrak
+
+type CategoryRepository interface {
+	Save(ctx context.Context, tx sql.Tx, category domain.Category) domain.Category
+	Update(ctx context.Context, tx sql.Tx, category domain.Category) domain.Category
+	Delete(ctx context.Context, tx sql.Tx, category domain.Category)
+	FindById(ctx context.Context, tx sql.Tx, id int) domain.Category
+	FindAll(ctx context.Context, tx sql.Tx) []domain.Category
+}
